4e2ea7bbd6b1eb19b789dc739cedfc63a505e28d
himanshuvb <savvysmith001@gmail.com>
2021-09-14 12:03:16 +0530

    mod in java

--- second.java
+++ second.java
@@ -1,2 +1,10 @@
-c = a-b
-print(c)
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
